f790790f1da132f56f3cb288c119319473cef397
Alex Cutts <alexcutts@getfieldfox.com>
2024-05-17 23:12:02 +0100

    Update 2024_02_10_000001_create_logins_table.php

--- database/migrations/2024_02_10_000001_create_logins_table.php
+++ database/migrations/2024_02_10_000001_create_logins_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('logins', function (Blueprint $table) {
             $table->id();
-            $table->morphs('authenticatable');
+            $table->ulidMorphs('authenticatable');
             $table->string('user_agent')->nullable();
             $table->string('ip_address')->nullable();
             $table->string('device_type')->nullable();
